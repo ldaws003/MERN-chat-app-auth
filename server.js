f8477cfd84e3a9770423efaee37f41239bd8a1f4
ldaws003 <ldaws003@fiu.edu>
2020-01-01 23:02:38 -0800

    added the MERN-Auth boilerplate

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+const dotenv = require('dotenv').config({path: process.cwd()+"/config/.env"});
+const passport = require("passport");
+
+const users = require("./routes/api/users.js");
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = process.env.DB;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true}
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+//Passport middleware 
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);  
+  
+  
+  
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
