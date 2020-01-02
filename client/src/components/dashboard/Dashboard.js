fca89b458c922738cd62962451ca2a81e8c7159f
ldaws003 <ldaws003@fiu.edu>
2020-01-02 02:02:29 -0800

    added socket.io and made a chatroom for the app

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+//import io from "../../../../node_modules/socket.io/socket.io.js";
+import io from "socket.io-client";
 
 
 class Dashboard extends Component {
    constructor(props){
       super(props);
+      this.state = {
+         text: "",
+         socket: io("http://localhost:5000"),
+         chatRoom: []
+      };
       this.onLogoutClick = this.onLogoutClick.bind(this);
+      this.onSubmit = this.onSubmit.bind(this); 
+      this.onChange = this.onChange.bind(this);
    }
    
    onLogoutClick = e => {
@@ -15,9 +24,28 @@ class Dashboard extends Component {
       this.props.logoutUser();
    };
    
+   onChange(e) {
+      this.setState({ text: e.target.value });
+   }
+   
+   componentDidMount() {
+      this.state.socket.on("chat message", (msg) => {
+         this.setState({ chatRoom: [...this.state.chatRoom, msg] })
+      });
+   }
+   
+   onSubmit(e){
+      e.preventDefault();
+      if (!this.state.text) {
+         // tell user to put in text
+         return;
+      }
+      this.state.socket.emit("chat message", this.state.text);
+      this.setState({ text: "" })
+   }
+   
    render() {
-      const { user } = this.props.auth;
-      
+      const { user } = this.props.auth;      
       return (
          <div style={{ height: "75vh" }} className="container valign-wrapper">
             <div className="row">
@@ -29,6 +57,31 @@ class Dashboard extends Component {
                         <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
                      </p>
                   </h4>
+                  {this.state.chatRoom.map((msg, i) => (<li key={msg + String(i)}><p>{msg}</p></li>))}
+                  <form 
+                     onSubmit={this.onSubmit}
+                     className="col s12">
+                     <div className="row">
+                        <div className="input-field col s12">
+                           <textarea 
+                              id="send-msg-text" 
+                              className="materialize-textarea" 
+                              placeholder="Say something..."
+                              onChange={this.onChange}
+                              value={this.state.text}/>
+                           <label htmlFor="send-msg-text" className="">Message</label> 
+                        </div>
+                        <input  
+                         type="submit"
+                         style={{
+                           borderRadius: "3px",
+                           letterSpacing: "1.5px",
+                           marginTop: "1rem"  
+                         }}
+                         className="btn waves-effect waves-light hoverable green accent-3"
+                         value="Send Message"/>
+                     </div>
+                  </form>
                   <button
                      style={{
                         width: "150px",
--- server.js
+++ server.js
@@ -4,16 +4,23 @@ const bodyParser = require("body-parser");
 const app = express();
 const dotenv = require('dotenv').config({path: process.cwd()+"/config/.env"});
 const passport = require("passport");
+const http = require('http').createServer(app);
+const io = require("socket.io").listen(http);
+const cors = require("cors");
 
 const users = require("./routes/api/users.js");
 
-// Bodyparser middleware
+// Bodyparser middleware and CORS
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
+app.use(cors({
+   origin: "http://localhost:3000",
+   credentials: true
+}));
 
 // DB Config
 const db = process.env.DB;
@@ -37,7 +44,19 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);  
   
-  
+// Socket.io
+io.on('connection', (socket) => {
+   console.log('a user connected');
+   
+   socket.on('chat message', (msg) => {
+      io.emit("chat message", msg);
+   });
+   
+   socket.on('disconnect', () => {
+      console.log('user disconnected');
+   });
+});
+
   
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+http.listen(port, () => console.log(`Server up and running on port ${port} !`));
